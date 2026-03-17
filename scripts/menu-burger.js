87ce708a2426f6334018a3a1e1104acdd2cdaf8f
Bruno <alvalade@live.fr>
2026-03-17 16:28:33 +0100

    compactage du burger script js et ajout du li nav__bar--title Menu

--- scripts/menu-burger.js
+++ scripts/menu-burger.js
@@ -1,31 +1,34 @@
+
 const burgerButton  = document.querySelector(".nav__burger-logo");
 const navBar = document.querySelector(".nav__bar");
 const navLinks = document.querySelectorAll(".nav__link")
 
+function closeMenu () {
+    navBar.classList.remove("nav__bar--ouvert");
+    burgerButton.setAttribute("aria-expanded", "false");
+}
+
 burgerButton.addEventListener("click" , function(event) {
-
-    navBar.classList.toggle ("nav__bar--ouvert");
-
+    navBar.classList.toggle("nav__bar--ouvert");
         const isExpanded = burgerButton.getAttribute ("aria-expanded") === "true";
-
-        burgerButton.setAttribute("aria-expanded", String (!isExpanded));
-
+        burgerButton.setAttribute("aria-expanded" , String (!isExpanded));
         event.stopPropagation();
+})
+
+navLinks.forEach(function(link) {
+    link.addEventListener("click", closeMenu);
 });
 
-navLinks.forEach(function (link) {
-    link.addEventListener("click", function () {
-        navBar.classList.remove("nav__bar--ouvert");
-        burgerButton.setAttribute("aria-expanded", "false");
-    });
-});
-
-document.addEventListener("click" , function (event) {
-    const clickMenu = navBar.contains(event.target)
+document.addEventListener("click", function (event) {
+    const clickMenu = navBar.contains(event.target);
     const clickBurger = burgerButton.contains(event.target)
-
     if (!clickMenu && !clickBurger) {
-        navBar.classList.remove("nav__bar--ouvert");
-        burgerButton.setAttribute("aria-expanded", "false");
+        closeMenu();
+    }
+});
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape" && navBar.classList.contains("nav__bar--ouvert")) {
+        closeMenu();
     }
 })
